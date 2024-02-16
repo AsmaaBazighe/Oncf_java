41a7a0450bc7586551a03b77ceb0107e6779c4d9
Rihab Id m'hand <148006167+rihabidm@users.noreply.github.com>
2024-02-16 15:49:44 +0100

    Update Main.java

--- oncf_project/src/codes/Main.java
+++ oncf_project/src/codes/Main.java
@@ -1,8 +1,55 @@
 package codes;
-import admin.Page1;
+
+import javax.swing.*;
+import java.awt.*;
+import admin.*;
 
 public class Main {
-	public static void main(String[] args) {
-        Page1.window();
+    public static void main(String[] args) {
+    	SwingUtilities.invokeLater(new Runnable() {
+    	    @Override
+    	    public void run() {
+    	        createAndShowGUI();
+    	    }
+    	});
+    }
+
+    private static void createAndShowGUI() {
+    	JFrame f = new JFrame();
+        f.setTitle("ONCF");
+        f.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        f.setResizable(false);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+
+        JTabbedPane tabbedPane = new JTabbedPane();
+
+        JPanel adminPanel = createAdminPanel();
+        JPanel userPanel = createUserPanel();
+
+        tabbedPane.addTab("Admin", adminPanel);
+        tabbedPane.addTab("User", userPanel);
+
+        f.getContentPane().add(tabbedPane, BorderLayout.CENTER);
+
+        
+        f.setVisible(true);
+    }
+
+    private static JPanel createAdminPanel() {
+    	JPanel panel = new JPanel();
+	    panel.setLayout(new BorderLayout());
+	    JPanel adminContentPanel = Login.window();
+	    panel.add(adminContentPanel, BorderLayout.CENTER);
+	    return panel;
+    }
+
+    private static JPanel createUserPanel() {
+    	    JPanel panel = new JPanel();
+    	    panel.setLayout(new BorderLayout());
+    	    JPanel userContentPanel = Page.window();
+    	    panel.add(userContentPanel, BorderLayout.CENTER);
+    	    return panel;
+    	
     }
 }
