305f5b3100d0d93e53e3d26498a3adfb82b85194
Asmaa BAZIGHE <156549687+AsmaaBazighe@users.noreply.github.com>
2024-02-16 17:52:26 +0100

    Update Main.java

--- oncf_project/src/codes/Main.java
+++ oncf_project/src/codes/Main.java
@@ -7,7 +7,7 @@ import admin.*;
 public class Main {
     public static void main(String[] args) {
     	SwingUtilities.invokeLater(new Runnable() {
-    	    @Override
+    	   
     	    public void run() {
     	        createAndShowGUI();
     	    }
