4a01c652b0d442aec83b481dc6df972512f2fa35
Rihab Id m'hand <148006167+rihabidm@users.noreply.github.com>
2024-02-16 15:53:28 +0100

    Update Page.java

--- oncf_project/src/codes/Page.java
+++ oncf_project/src/codes/Page.java
@@ -3,12 +3,7 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 public class Page {
-	public static void window() {
-        JFrame f = new JFrame();
-        f.setTitle("ONCF");
-        f.setExtendedState(JFrame.MAXIMIZED_BOTH);
-        f.setResizable(false);
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	public static JPanel window() {
 
         JPanel mainPanel = new JPanel();
         JButton b1 = new JButton("J’achète mon billet avec tarif normal");
@@ -25,7 +20,6 @@ public class Page {
         Styles.centerButtons(mainPanel, b1,b2);
         
         
-        f.setContentPane(mainPanel);
-        f.setVisible(true);
+        return mainPanel;
     }
 }
