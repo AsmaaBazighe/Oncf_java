bda1e1ec3698c6d3a386dfb18a01166011f06a12
linamouissou <157241464+linamouissou@users.noreply.github.com>
2024-02-17 01:09:45 +0100

    Update Login.java

--- oncf_project/src/admin/Login.java
+++ oncf_project/src/admin/Login.java
@@ -4,22 +4,26 @@ import codes.*;
 import java.sql.*;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
 import java.util.ArrayList;
 
+
 public class Login {
-    
+	
     public static JPanel window() {
+        
+
         JPanel mainPanel = new JPanel(new GridBagLayout());
         Styles.bgColor(mainPanel);
-        mainPanel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+
+        
+        mainPanel.setBorder(BorderFactory.createEmptyBorder(20, 20,20, 20));
 
         Statement st = Connexion.connectONCF();
         final ArrayList<User> data = new ArrayList<User>();
         try {
-            ResultSet res = st.executeQuery("select * from admin");
+            ResultSet res = st.executeQuery("select * from a");
             while (res.next()) {
-                String username = res.getString(1);
+            	String username = res.getString(1);
                 String password = res.getString(2);
                 User user = new User(username, password);
                 data.add(user);
@@ -27,43 +31,23 @@ public class Login {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
-        final JTextField userField = new JTextField(10);
-        final JPasswordField passwordField = new JPasswordField(10);
+        
+        
+        final JTextField user = new JTextField();
+        user.setColumns(10);
+        final JPasswordField psw = new JPasswordField();
+        psw.setColumns(10);
         JButton submitButton = new JButton("Soumettre");
-
-        Styles.organiserPay(mainPanel, submitButton, userField, passwordField);
-
-        submitButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                String enteredUsername = userField.getText();
-                String enteredPassword = new String(passwordField.getPassword());
-
-                boolean authenticated = false;
-                for (User u : data) {
-                    if (u.getUsername().equals(enteredUsername) && u.getPassword().equals(enteredPassword)) {
-                        authenticated = true;
-                        break;
-                    }
-                }
-
-                if (authenticated) {
-                    JOptionPane.showMessageDialog(null, "Connexion réussie !");
-                   
-                    GoTo.choix(null); 
-                } else {
-                    JOptionPane.showMessageDialog(null, "Nom d'utilisateur ou mot de passe incorrect !");
-                }
-            }
-        });
-
+        Styles.organiserPay(mainPanel,submitButton,user,psw);
+        GoTo.choix(submitButton);
+        
         return mainPanel;
     }
+    
+    
 
     
-}
-    
+} 
     
 
     
